ec5e9e6a0d2fb96a05c3ae2124db64d4aaa4a388
zingoworks <zingoworks@gmail.com>
2021-02-23 00:28:17 +0900

    crypto-js 추가, 기타 함수 정의

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js"
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string =>
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index: number,
         hash: string,
@@ -21,7 +32,13 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "2ajdaj299", "", "hello", 123456)
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date(0).getTime() / 1000);
 
 console.log(blockchain);
 
