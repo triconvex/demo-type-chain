52fc4d1cf1ea3c842929463ff9ab71ed82016d9a
zingoworks <zingoworks@gmail.com>
2021-02-22 21:18:51 +0900

    타입스크립트로 함수 만들기, with argument?

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "anonymous", age = 99, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+// sayHi(name, age, gender);
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+const name = "anonymous",
+age = 99,
+gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+// sayHi(name, age, gender);
+sayHi(name, age);
+
+export {};
