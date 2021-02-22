ef6dd5660d8671842ace8ab80f75a8f99859efd4
zingoworks <zingoworks@gmail.com>
2021-02-22 23:38:51 +0900

    TS의 인터페이스

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
 }
 
-console.log(sayHi("anonymous", 199, "male"));
+const person = {
+    name: "anonymous",
+    age: 99,
+    gender: "male"
+}
+
+// const sayHi = (name: string, age: number, gender: string): string => {
+//     return `Hello ${name}, you are ${age}, you are a ${gender}`;
+// }
+
+// console.log(sayHi("anonymous", 199, "male"));
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+}
+
+console.log(sayHi(person));
+
 
 export { };
