d5fac224106f15709b27cda7e8d659165945dd5d
zingoworks <zingoworks@gmail.com>
2021-02-22 21:35:02 +0900

    함수의 리턴타입 지정, tsc-watch를 통해 생산성 높이기

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "anonymous", age = 99, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-// sayHi(name, age, gender);
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
